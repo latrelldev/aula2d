c8c28c0a9ec95b01841c5dfaa71602b75a5e472b
migueltavares <miguel.tavares@manifestogames.com.br>
2024-11-04 11:56:02 -0300

    add bullet movement

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField]
+    private float _bulletSpeed;
+
+    [SerializeField]
+    private Transform _gun;
+
+    [SerializeField]
+    private float _timeshot;
+
+    private bool _fireContinuosly;
+    private float _lastFireTime;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_fireContinuosly)
+        {
+            float timeSinceLastFire = Time.time - _lastFireTime;
+
+            if(timeSinceLastFire >= _timeshot)
+            {
+                FireBullet();
+                _lastFireTime = Time.time;
+            }
+        }
+
+    }
+
+    private void FireBullet()
+    {
+        GameObject bullet = Instantiate(_bulletPrefab, _gun.position, _gun.rotation);
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = _bulletSpeed * transform.up;
+    }
+    private void OnFire (InputValue inputValue)
+    {
+        _fireContinuosly = inputValue.isPressed;
+    }
+    
+}
